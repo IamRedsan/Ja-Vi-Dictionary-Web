d4c547c8375e3cd09ff9df8975c8687fb2333d86
Võ Viết Trường <voviettruong141003@gmail.com>
2024-12-23 00:13:44 +0700

    user-profile ui

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -9,28 +9,21 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
-import { Label } from '@/components/ui/label';
-import { Switch } from '@/components/ui/switch';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
+
 import { useSidebar } from '@/hooks/use-sidebar';
 import { useStore } from '@/hooks/use-store';
+import Profile from '@/components/profile/Profile';
 
 export default function ProfilePage() {
   const sidebar = useStore(useSidebar, (x) => x);
   if (!sidebar) return null;
-  const { settings, setSettings } = sidebar;
   return (
-    <ContentLayout title='Dashboard'>
+    <ContentLayout title='Thông tin quản lý'>
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
             <BreadcrumbLink asChild>
-              <Link href='/'>Home</Link>
+              <Link href='/'>Trang chủ</Link>
             </BreadcrumbLink>
           </BreadcrumbItem>
           <BreadcrumbSeparator />
@@ -39,40 +32,9 @@ export default function ProfilePage() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <TooltipProvider>
-        <div className='flex gap-6 mt-6'>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <div className='flex items-center space-x-2'>
-                <Switch
-                  id='is-hover-open'
-                  onCheckedChange={(x) => setSettings({ isHoverOpen: x })}
-                  checked={settings.isHoverOpen}
-                />
-                <Label htmlFor='is-hover-open'>Hover Open</Label>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>When hovering on the sidebar in mini state, it will open</p>
-            </TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <div className='flex items-center space-x-2'>
-                <Switch
-                  id='disable-sidebar'
-                  onCheckedChange={(x) => setSettings({ disabled: x })}
-                  checked={settings.disabled}
-                />
-                <Label htmlFor='disable-sidebar'>Disable Sidebar</Label>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>Hide sidebar</p>
-            </TooltipContent>
-          </Tooltip>
-        </div>
-      </TooltipProvider>
+      <div className=''>
+        <Profile className='w-[600px]' isVerify />
+      </div>
     </ContentLayout>
   );
 }
--- app/(main)/user/page.tsx
+++ app/(main)/user/page.tsx
@@ -9,28 +9,186 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
-import { Label } from '@/components/ui/label';
-import { Switch } from '@/components/ui/switch';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
-import { useSidebar } from '@/hooks/use-sidebar';
-import { useStore } from '@/hooks/use-store';
-
+import { DataTable } from '@/components/table/data-table';
+import { columns, IUser } from '@/components/table/user-rows';
+const generateISODate = (): string => {
+  return new Date().toISOString();
+};
 export default function UserPage() {
-  const sidebar = useStore(useSidebar, (x) => x);
-  if (!sidebar) return null;
-  const { settings, setSettings } = sidebar;
+  const data: IUser[] = [
+    {
+      _id: '1',
+      email: 'johndoe@example.com',
+      username: 'johndoe',
+      fullname: 'John Doe',
+      avatar: 'https://i.pravatar.cc/150?img=1',
+      role: 'user',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '2',
+      email: 'janedoe@example.com',
+      username: 'janedoe',
+      fullname: 'Jane Doe',
+      avatar: 'https://i.pravatar.cc/150?img=2',
+      role: 'admin',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: false,
+    },
+    {
+      _id: '3',
+      email: 'bobjohnson@example.com',
+      username: 'bobjohnson',
+      fullname: 'Bob Johnson',
+      avatar: 'https://i.pravatar.cc/150?img=3',
+      role: 'user',
+      isBanned: true,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '4',
+      email: 'alicewong@example.com',
+      username: 'alicewong',
+      fullname: 'Alice Wong',
+      avatar: 'https://i.pravatar.cc/150?img=4',
+      role: 'user',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '5',
+      email: 'michaelbrown@example.com',
+      username: 'michaelbrown',
+      fullname: 'Michael Brown',
+      avatar: 'https://i.pravatar.cc/150?img=5',
+      role: 'admin',
+      isBanned: true,
+      createdAt: generateISODate(),
+      isVerify: false,
+    },
+    {
+      _id: '1',
+      email: 'johndoe@example.com',
+      username: 'johndoe',
+      fullname: 'John Doe',
+      avatar: 'https://i.pravatar.cc/150?img=1',
+      role: 'user',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '2',
+      email: 'janedoe@example.com',
+      username: 'janedoe',
+      fullname: 'Jane Doe',
+      avatar: 'https://i.pravatar.cc/150?img=2',
+      role: 'admin',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: false,
+    },
+    {
+      _id: '3',
+      email: 'bobjohnson@example.com',
+      username: 'bobjohnson',
+      fullname: 'Bob Johnson',
+      avatar: 'https://i.pravatar.cc/150?img=3',
+      role: 'user',
+      isBanned: true,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '4',
+      email: 'alicewong@example.com',
+      username: 'alicewong',
+      fullname: 'Alice Wong',
+      avatar: 'https://i.pravatar.cc/150?img=4',
+      role: 'user',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '5',
+      email: 'michaelbrown@example.com',
+      username: 'michaelbrown',
+      fullname: 'Michael Brown',
+      avatar: 'https://i.pravatar.cc/150?img=5',
+      role: 'admin',
+      isBanned: true,
+      createdAt: generateISODate(),
+      isVerify: false,
+    },
+    {
+      _id: '1',
+      email: 'johndoe@example.com',
+      username: 'johndoe',
+      fullname: 'John Doe',
+      avatar: 'https://i.pravatar.cc/150?img=1',
+      role: 'user',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '2',
+      email: 'janedoe@example.com',
+      username: 'janedoe',
+      fullname: 'Jane Doe',
+      avatar: 'https://i.pravatar.cc/150?img=2',
+      role: 'admin',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: false,
+    },
+    {
+      _id: '3',
+      email: 'bobjohnson@example.com',
+      username: 'bobjohnson',
+      fullname: 'Bob Johnson',
+      avatar: 'https://i.pravatar.cc/150?img=3',
+      role: 'user',
+      isBanned: true,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '4',
+      email: 'alicewong@example.com',
+      username: 'alicewong',
+      fullname: 'Alice Wong',
+      avatar: 'https://i.pravatar.cc/150?img=4',
+      role: 'user',
+      isBanned: false,
+      createdAt: generateISODate(),
+      isVerify: true,
+    },
+    {
+      _id: '5',
+      email: 'michaelbrown@example.com',
+      username: 'michaelbrown',
+      fullname: 'Michael Brown',
+      avatar: 'https://i.pravatar.cc/150?img=5',
+      role: 'admin',
+      isBanned: true,
+      createdAt: generateISODate(),
+      isVerify: false,
+    },
+  ];
   return (
-    <ContentLayout title='Dashboard'>
+    <ContentLayout title='Danh sách người dùng'>
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
             <BreadcrumbLink asChild>
-              <Link href='/'>Home</Link>
+              <Link href='/'>Trang chủ</Link>
             </BreadcrumbLink>
           </BreadcrumbItem>
           <BreadcrumbSeparator />
@@ -39,40 +197,9 @@ export default function UserPage() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <TooltipProvider>
-        <div className='flex gap-6 mt-6'>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <div className='flex items-center space-x-2'>
-                <Switch
-                  id='is-hover-open'
-                  onCheckedChange={(x) => setSettings({ isHoverOpen: x })}
-                  checked={settings.isHoverOpen}
-                />
-                <Label htmlFor='is-hover-open'>Hover Open</Label>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>When hovering on the sidebar in mini state, it will open</p>
-            </TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <div className='flex items-center space-x-2'>
-                <Switch
-                  id='disable-sidebar'
-                  onCheckedChange={(x) => setSettings({ disabled: x })}
-                  checked={settings.disabled}
-                />
-                <Label htmlFor='disable-sidebar'>Disable Sidebar</Label>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>Hide sidebar</p>
-            </TooltipContent>
-          </Tooltip>
-        </div>
-      </TooltipProvider>
+      <div className='container mx-auto py-10'>
+        <DataTable columns={columns} data={data} />
+      </div>
     </ContentLayout>
   );
 }
